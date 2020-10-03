26fbcd9ebf30df067924d05ecd49dd6f747d1762
Gauri Paliwal <45237431+paliwalgauri@users.noreply.github.com>
2020-10-03 21:29:38 +0530

    Add files via upload

---
+++ product.php
@@ -0,0 +1,275 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="description" content="Women Fashion Shopping Online - Shop from a huge range of latest women clothing, shoes, makeup Kits, Watches, footwear and more for women at best price in India @ Jabong from Top Brands. Best Online Fashion Shopping *COD *15-Days Returns *Free Shipping.">
+  <meta name="keywords" content="Online Shopping Website For Women, Women Fashion Shop, Buy Womens Clothes, Women Apparel Online Shopping India, Ladies Fashion Clothing">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<!--===============================================================================================-->
+
+
+    <title>Cheap Shop</title>
+    
+</head>
+<body>
+    
+    <!-- Head  Section Start -->
+    <section class="head">
+        <div class="row" style="width: 100%;">
+            <div class="col-md-12" style="margin-top:0px;margin-bottom:0px;">
+                <div class="headerlink col-md-4 col-xs-12" style="margin-top: 5px;">
+                    <h5 align="center"><big>&#128383; +91 8442285358</big></h5>   
+                </div>
+                <div class="col-md-4 col-xs-12" style="margin-top: 5px;">
+                    <div class="feature-box" >
+                        <div class="icons"> </div>
+                        <center><big><h5 align="center"><big><big>&#9993;</big> &nbsp; Email: </b> support@cheapshop.com</big></h5>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <div class="feature-box">
+                        <a href="#" style="text-decoration:none; color:white; ">   <div class="admission_button"><h5 align="center"><b> Download App</b></h5></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Head Section End -->
+
+    <!-- NAVIGATION BAR START  -->
+        <nav class="navbar navbar-default">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="index.php" class="navbar-brand">Home Services</a>
+            </div>
+            <!-- Collection of nav links and other content for toggling -->
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="index.php/#trending">Services</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><input type="text" placeholder="Search" class="search"></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">&#128722;</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    <!-- NAVIGATION BAR END -->
+
+    <!-- SLIDE BAR START -->
+
+        <div class="carousel" >
+          
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+              
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" >
+
+              <div class="item active">
+                <img src="assets/images/banner1.png" id="slider_image"   >
+                <div class="carousel-caption">
+                  <h3></h3>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="assets/images/banner2.png"  class="slider_image" >
+                <div class="carousel-caption">
+                  <h3></h3>
+                  </div>
+              </div>
+            
+              <div class="item">
+                <img src="assets/images/banner3.png" class="slider_image" >
+                <div class="carousel-caption">
+                  <h3></h3>
+                  </div>
+              </div>
+               
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+    <!-- SLIDER BAR END -->
+
+
+
+<!-- Contact Us Start -->
+
+<section class="contact">
+<div class="row" style="margin-bottom: 25px;">
+        <div class="col-md-12"  >
+    <div class=" col-md-9 col-xs-12" style="background: white;">
+        <div>
+            <div style="margin-top: 25px;">
+                        <div class="form-group">
+                            <h1>Ac Servicing</h1>
+                            <label for="name">Inclusions & Exclusions</label>
+                            <ol>
+              <li> Cooling of the AC depends on multiple factors such as quality of the gas-filled, age of the AC, pressure of the gas etc, and servicing alone does not guarantee improvement in the cooling</li>
+              <li>Prices shown are labor charges only</li>
+              <li>We provides a 30 day warranty on the service provided</li>
+              <li>If any repair work is required, a quote will be given before proceeding</li>
+              <li>We strongly discourage direct dealing with our service provider regarding a service booked through us All warranty and service assurance in such cases stand null and void</li>
+              <li>Consumables and parts (if used) will be charged extra</li>
+              <li>Warranty on consumables and parts will be as per manufacturer only</li>
+                           
+            </ol>
+            <label for="name">Procesure</label>
+                            <ol>
+              <li>Gas pressure check before start of service</li>
+              <li>Check whether the AC in in working condition</li>
+              <li>Washing and cleaning of filters</li>
+              <li>Cleaning of blower wheels and blades</li>
+              <li>Cleaning of indoor cooler drain tray</li>
+              <li>Cleaning of water condensation drainage system</li>
+              <li>Thorough inspection of the AC unit to check for any leakages in the coils and drain pipeline. In case of an internal leakage, Nitrogen test needs to be done to identify location of leakage, charges for which will be extra</li>
+              <li>Check settings of the AC</li>
+              <li>Reinstalling of indoor cooler units</li>
+              <li>Gas top up , if required and cost approved by the customer</li>
+              <li>Performance check</li>
+              
+            </ol>
+                        </div>
+                        
+                    </div>
+
+        </div>
+    </div>
+    <div class=" col-md-3 col-xs-12" >
+        <div>
+            <div class="card-header bg-success text-white text-uppercase" style="margin-top: 25px; padding: 5px;"><i class="fa fa-home"></i> Address</div>
+                <div class="card-body"><Br><br>
+                    <p>1 N. Rock Creek Ave.</p>
+                    <p>Danville, VA 24540</p>
+                    <p>Email- support@homeservices.com</p>
+                    
+                    
+
+                </div>
+
+        </div>
+    </div>
+</div>
+</div>
+</section>
+<!-- Contact Us End -->
+
+<!-- Start Footer -->
+<section class="footer">
+<div class="row" style="background:#EEEEEE;">   
+    <div class="col-md-12"  >
+    <div class=" col-md-3 col-xs-12" >
+        <div>
+            <h3>ONLINE SHOPPING</h3>
+            <a href="#">Ac</a><br>
+            <a href="#">Refrigerator</a><br>
+            <a href="#">Tv</a><br>
+            <a href="#">Washing Machine</a><br>
+            <a href="#">Water Heater</a><br>
+            
+
+        </div>
+    </div>
+    <div class=" col-md-3 col-xs-12" >
+        <div>
+            <h3>USEFUL LINKS</h3>
+            <a href="#">Contact Us</a><br>
+            <a href="#">FAQ</a><br>
+            <a href="#">T&C</a><br>
+            <a href="#">Terms Of Use</a><br>
+            <a href="#">Track Orders</a><br>
+            <a href="#">Shipping</a><br>
+            <a href="#">Cancellation</a><br>
+            <a href="#">Returns</a><br>
+            <a href="#">Whitehat</a><br>
+            <a href="#">Blog</a><br>
+            <a href="#">Careers</a><br>
+        </div>
+
+    </div>
+    <div class=" col-md-3 col-xs-12" >
+        <div>
+            <h3>Our Applications For Mobile</h3>
+                <div class=" col-md-6 col-xs-12" >
+                    <div>
+                        <center><img src="assets/images/play.png" alt="App" width="100%"><center>
+                    </div>
+                </div>
+                <div class=" col-md-6 col-xs-12" >
+                    <div>
+                        <center><img src="assets/images/app.png" alt="App"  width="100%"><center>
+                    </div>
+                </div>
+                
+                <div class=" col-md-12 col-xs-12" >
+                    <div>
+                        <div style="text-align:center;"><script type="text/javascript" src="http://services.webestools.com/cpt_visitors/61624-13-5.js"></script></div><a href="http://www.webestools.com/" style="display:block;text-align:center;" title="Tools services webmasters counters generators scripts tutorials free"></a>
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+    <div class=" col-md-3 col-xs-12" >
+        <div>
+            <h3> </h3>
+            <table style="padding:5px;">
+                <tr><td>
+                <img src="assets/images/original.png" alt="Cheap Shop"></td>
+                <td><b>100%  Satisfaction Guarntee </b> <br><br></Td></tr>
+                <td><img src="assets/images/return.png" alt="Cheap Shop"></td><td>24X7 Support<br>
+                
+                <br></td></tr> 
+                <td><img src="assets/images/delievery.png" alt="Cheap Shop"></td><td><b>Get Assured Support </b>  </td></tr></table>
+        </div>
+    </div>
+    
+</div>  
+</section>
+
+<!-- End Footer -->
+
+</div>
+
+    <!-- IMAGES END -->
+<div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;"><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&utm_campaign=000_logo&utm_medium=website&utm_content=footer_img"><img src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png" alt="www.000webhost.com"></a></div><script>function getCookie(e){for(var t=e+"=",n=decodeURIComponent(document.cookie).split(";"),o=0;o<n.length;o++){for(var i=n[o];" "==i.charAt(0);)i=i.substring(1);if(0==i.indexOf(t))return i.substring(t.length,i.length)}return""}getCookie("hostinger")&&(document.cookie="hostinger=;expires=Thu, 01 Jan 1970 00:00:01 GMT;",location.reload());var notification=document.getElementsByClassName("notice notice-success is-dismissible"),hostingerLogo=document.getElementsByClassName("hlogo"),mainContent=document.getElementsByClassName("notice_content")[0],newList=["Powerful and Easy-To-Use Control Panel.","1-Click Auto Installer and 24/7 Live Support.","Free Domain, Email and SSL Bundle.","5x faster WordPress performance","Weekly Backups and Fast Response Time."];if(0<notification.length&&null!=mainContent){var googleFont=document.createElement("link");googleFontHref=document.createAttribute("href"),googleFontRel=document.createAttribute("rel"),googleFontHref.value="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600",googleFontRel.value="stylesheet",googleFont.setAttributeNode(googleFontHref),googleFont.setAttributeNode(googleFontRel);var css="@media only screen and (max-width: 768px) {.web-hosting-90-off-image-wrapper {position: absolute;} .notice_content {justify-content: center;} .web-hosting-90-off-image {opacity: 0.3;}} @media only screen and (min-width: 769px) {.notice_content {justify-content: space-between;} .web-hosting-90-off-image-wrapper {padding: 0 5%}} .content-wrapper {z-index: 5} .notice_content {display: flex; align-items: center;} * {-webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale;} .upgrade_button_red_sale{border: 0; border-radius: 3px; background-color: #ff123a !important; padding: 15px 55px !important; margin-left: 30px; font-family: 'Open Sans', sans-serif; font-size: 16px; font-weight: 600; color: #ffffff;} .upgrade_button_red_sale:hover{color: #ffffff !important; background: #d10303 !important;}",style=document.createElement("style"),sheet=window.document.styleSheets[0];style.styleSheet?style.styleSheet.cssText=css:style.appendChild(document.createTextNode(css)),document.getElementsByTagName("head")[0].appendChild(style),document.getElementsByTagName("head")[0].appendChild(googleFont);var button=document.getElementsByClassName("upgrade_button_red")[0],link=button.parentElement;link.setAttribute("href","https://www.hostinger.com/hosting-starter-offer?utm_source=000webhost&utm_medium=panel&utm_campaign=000-wp"),link.innerHTML='<button class="upgrade_button_red_sale">TRANSFER NOW</button>',(notification=notification[0]).setAttribute("style","padding-bottom: 10px; padding-top: 5px; background-image: url(https://cdn.000webhost.com/000webhost/promotions/springsale/mountains-neon-background.jpg); background-color: #000000; background-size: cover; background-repeat: no-repeat; color: #ffffff; border-color: #ff123a; border-width: 8px;"),notification.className="notice notice-error is-dismissible",(hostingerLogo=hostingerLogo[0]).setAttribute("src","https://cdn.000webhost.com/000webhost/promotions/springsale/logo-hostinger-white.svg"),hostingerLogo.setAttribute("style","float: none !important; height: auto; max-width: 100%; margin: 40px 20px 10px 30px;");var h1Tag=notification.getElementsByTagName("H1")[0];h1Tag.remove();var paragraph=notification.getElementsByTagName("p")[0];paragraph.innerHTML="Migrate your WordPress to Hostinger and enjoy the best features on the market! The time is now!",paragraph.setAttribute("style",'max-width: 600px; margin-left: 30px; font-family: "Open Sans", sans-serif; font-size: 16px; font-weight: 600;');var list=notification.getElementsByTagName("UL")[0];list.setAttribute("style","max-width: 675px;");for(var listElements=list.getElementsByTagName("LI"),i=0;i<newList.length;i++)listElements[i].setAttribute("style","color:#ffffff; list-style-type: disc; margin-left: 30px; font-family: 'Open Sans', sans-serif; font-size: 14px; font-weight: 300; line-height: 1.5;"),listElements[i].innerHTML=newList[i];listElements[listElements.length-1].remove();var org_html=mainContent.innerHTML,new_html='<div class="content-wrapper">'+mainContent.innerHTML+'</div><div class="web-hosting-90-off-image-wrapper"><img class="web-hosting-90-off-image" src="https://cdn.000webhost.com/000webhost/promotions/springsale/web-hosting-90-off.png"></div>';mainContent.innerHTML=new_html;var saleImage=mainContent.getElementsByClassName("web-hosting-90-off-image")[0];!function(){var t=document.querySelectorAll("body.wp-admin")[0];function e(){var e=document.createElement("iframe");e.id="hgr-promo-widget",e.setAttribute("src","https://www.hostinger.com/widgets/bottom-banner-sale/000_wp_admin"),e.setAttribute("allowfullscreen",!0),e.setAttribute("frameborder",0),e.style.cssText="z-index: 2147483000 !important;position: fixed !important;bottom: 0; width: 100%;!important; left: 0!important;",e.style.opacity=0,e.onload=function(){iFrameResize({},"#hgr-promo-widget"),e.style.opacity=1},t.insertAdjacentElement("afterend",e)}if(window.iFrameResize)e();else{var n=document.createElement("script");n.type="text/javascript",t.insertAdjacentElement("afterend",n),n.onload=e,n.src="https://unpkg.com/iframe-resizer@3.6.3/js/iframeResizer.min.js"}}()}</script></body>
+</html>
